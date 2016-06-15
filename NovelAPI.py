f903ef98b388fc6d8f7b84982acef69f28c27e5f
GetRektByMe <GetRektByMe@users.noreply.github.com>
2016-06-15 12:26:20 +0100

    Data changed to fix beginner error.
    Data before was defined as empty and then everything was added indiviudally, now is all added at creation

--- NovelAPI.py
+++ NovelAPI.py
@@ -2,6 +2,8 @@ import aiohttp
 from bs4 import BeautifulSoup
 
 onlysession = aiohttp.ClientSession()
+
+
 class NovelUpdatesAPI:
     def __init__(self):
         self.baseurl = 'http://www.novelupdates.com/'
@@ -23,26 +25,25 @@ class NovelUpdatesAPI:
             async with session.get(to_parse) as response:
                 assert isinstance(response, aiohttp.ClientResponse)
                 assert response.status == 200
-            data = {}
             parse_info = BeautifulSoup(await response.text(), 'lxml')
-            data['title'] = parse_info.find(class_='seriestitle new')
-            data['cover'] = parse_info.find('img').get('src')
-            data['type'] = parse_info.find('a', class_='genre type').text()
-            data['genre'] = parse_info.find_all('a', class_='genre').text()
-            data['tags'] = parse_info.find_all('a', class_='genre odd').text()
-            data['rating'] = parse_info.find('span', class_='votetext').text()
-            data['language'] = parse_info.find('a', class_='genre lang').text()
-            data['author'] = parse_info.find('a', class_='authtag').text()
-            data['artist'] = parse_info.find('a', class_='artiststag').text()
-            data['year'] = parse_info.find('div', id_='edityear').text()
-            data['novel_status'] = parse_info.find('div', id_='editstatus').text()
-            data['licensed'] = parse_info.find('div', id_='showlicensed').text()
-            data['completely_translated'] = parse_info.find('div', id_='showtranslated').text()
-            data['publisher'] = parse_info.find('a', class_='genre', id_='myopub').text()
-            data['english_publisher'] = parse_info.find('span', class_='seriesna').text()
-            data['frequency'] = parse_info.find('h5', class_='seriesother').text()
-            data['description'] = parse_info.find('div', id_='editdescription').text().strip()
-            data['aliases'] = parse_info.find('div', id_='editassociated').text()
-            data['related'] = parse_info.find('h5', class_='seriesother').text()
+            data = {'title': parse_info.find(class_='seriestitle new'),
+                'cover': parse_info.find('img').get('src'),
+                'type': parse_info.find('a', class_='genre type').text(),
+                'genre': parse_info.find_all('a', class_='genre').text(),
+                'tags': parse_info.find_all('a', class_='genre odd').text(),
+                'rating': parse_info.find('span', class_='votetext').text(),
+                'language': parse_info.find('a', class_='genre lang').text(),
+                'author': parse_info.find('a', class_='authtag').text(),
+                'artist': parse_info.find('a', class_='artiststag').text(),
+                'year': parse_info.find('div', id_='edityear').text(),
+                'novel_status': parse_info.find('div', id_='editstatus').text(),
+                'licensed': parse_info.find('div', id_='showlicensed').text(),
+                'completely_translated': parse_info.find('div', id_='showtranslated').text(),
+                'publisher': parse_info.find('a', class_='genre', id_='myopub').text(),
+                'english_publisher': parse_info.find('span', class_='seriesna').text(),
+                'frequency': parse_info.find('h5', class_='seriesother').text(),
+                'description': parse_info.find('div', id_='editdescription').text().strip(),
+                'aliases': parse_info.find('div', id_='editassociated').text(),
+                'related': parse_info.find('h5', class_='seriesother').text()}
 
             return data
