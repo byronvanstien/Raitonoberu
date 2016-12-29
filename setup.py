aedc7b99acf9526e51a59a281ab3ad2622bdf424
rachmadaniHaryono <foreturiga@gmail.com>
2016-12-29 09:16:15 +0800

    change requirements to install.

--- setup.py
+++ setup.py
@@ -9,7 +9,10 @@ setup(
     author='Recchan',
     author_email='',
     description='Python 3 Aiohttp Novelupdates Scraper',
-    long_description='Raitonoberu is a Python 3 Asyncio NovelUpdates Website scraper that is capable of getting useful information for use in applications.',
+    long_description=(
+        'Raitonoberu is a Python 3 Asyncio NovelUpdates Website scraper '
+        'that is capable of getting useful information for use in applications.'
+    ),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -19,6 +22,4 @@ setup(
         'Topic :: Software Development :: Libraries'
     ],
     keywords="NovelUpdates asyncio aiohttp scraping",
-    install_requires=['aiohttp', 'bs4', 'lxml'],
-
 )
